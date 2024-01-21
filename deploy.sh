614dc7a3dedd9daae5fe727e4e9ad7616069e32d
Lukas Nitzsche <lukasnitzsche@yahoo.com>
2024-01-21 20:02:08 +0100

    udpate

--- deploy.sh
+++ deploy.sh
@@ -1,33 +1,14 @@
 #!/bin/sh
 
-# deploy.sh install
-# deploy.sh update
-# deploy.sh help
 #
-# Things to deploy and install on system
-#   For all distros (maybe seperate for xorg and wayland)?
-#   - X11 things
-#       - xorg-server, xinput, xorg-init, .xinitrc,
-#       - xcpip, xev, xbacklight (add user to group video)
-#       - xrandr/autorandr/arandr and setup
-#       - /etc/X11/xorg.conf.d/00-keyboard.conf
-#       - /usr/share/X11/xkb/symbols/pc
-#       - /etc/systemd/system/slock@.service
-#       - dwm, dmenu, slock, clipmenu,
-#   - iosevka font
-#   - tlp and setup
-#   - starship prompt
-#   - network manager, nm-applet
-#   - alacritty, sxiv, vifm, zathura,
-#   - playerctl
-#   - scrot
+# Configs
+# - nvim, alacritty, tmux, vifm, zathura, zsh
+#   cmus
 #
-#   For personal Arch system
-#   - paru
-#   - Packages to install
+# Scripts
+# - dragon, fet, pfetch, qrcode.sh
 #
-#   - scripts, zsh, chsh
-#   - alacrity, vifm, vifmrun, nvim, tmux, zathura, cmus
+# Packages (based on system)
+# - neovim, alacritty, tmux, vifm, zathura, zsh, sxiv
+#   qrencode, cmus
 #
-# fix suckles config.def.h and make file not delete config.h
-# Add some patches to dwm (layouts, etc.)
