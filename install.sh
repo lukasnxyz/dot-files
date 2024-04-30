6f5cd192b40f0ffd269ae631166e6350f7f8c250
Lukas Nitzsche <lukasnitzsche@yahoo.com>
2024-04-30 20:10:35 +0200

    editing and testing with docker

--- install.sh
+++ install.sh
@@ -27,15 +27,15 @@ export XDG_CACHE_HOME=${CACHE_DIR}
 # check if laptop and if so, install auto-cpufreq?
 # (check if current dir is dot-files) git clone this repo: https://github.com/lukasnxyz/dot-files
 
-printf "%s%s%s\n" $COLOR_GREEN "2. Installing packages: ${packages}" $COLOR_RESET
 packages="zsh alacritty neovim tmux"
+printf "%s%s%s\n" $COLOR_GREEN "2. Installing packages: ${packages}" $COLOR_RESET
 #dev_packages="python3" # add all development packages 
 sudo apt-get update 
 sudo apt-get install -y ${packages} # Ubuntu(apt), Arch(pacman), Fedora(dnf)
 
-printf "%s%s%s\n" $COLOR_GREEN "3. Installing configuration files" $COLOR_RESET
 config=("alacritty/", "nvim/", "tmux/", "zsh/")
 home=(".zshenv")
+printf "%s%s%s\n" $COLOR_GREEN "3. Installing configuration files" $COLOR_RESET
 for c in "${config[@]}"
 do
 	cp -v -r ${c} ${CONFIG_DIR} # commas after each is messing up
