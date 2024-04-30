301043ef18911e3e336a1e463743b14c6e8204a9
Lukas Nitzsche <lukasnitzsche@yahoo.com>
2024-04-30 19:24:34 +0200

    editing and testing with docker

--- install.sh
+++ install.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
+COLOR_RESET="$(tput sgr0)"
+COLOR_GREEN="$(tput setaf 2)"
+COLOR_RED="$()"
+
 # check if XDG things are set
 # check if laptop and if so, install auto-cpufreq?
 # (check if current dir is dot-files) git clone this repo: https://github.com/lukasnxyz/dot-files
-COLOR_GREEN="$(tput setaf 2)"
-printf "%s%s\n" $COLOR_GREEN "Installing dot-files"
 
-echo "\t\033[1;32m1. Installing packages: ${packages}"
+printf "%s%s%s\n" $COLOR_GREEN "Installing dot-files" $COLOR_RESET
+
+printf "%s%s%s\n" $COLOR_GREEN "\n\t1. Installing packages: ${packages}" $COLOR_RESET
 packages="zsh alacritty neovim tmux"
 sudo apt install -y ${packages} # Ubuntu(apt), Arch(pacman), Fedora(dnf)
 
-echo "\t\033[1;32m2. Installing configuration files"
+printf "%s%s%s\n" $COLOR_GREEN "\n\t2. Installing configuration files" $COLOR_RESET
 configs=("alacritty/", "nvim/", ".zshenv", "zsh/", "tmux/")
 
 #packages_install=""
@@ -21,5 +25,5 @@ configs=("alacritty/", "nvim/", ".zshenv", "zsh/", "tmux/")
 #	packages_install="${packages_install}${p} " 
 #done
 
-echo "\t\033[1;32m3. Changing default shell to zsh"
+printf "%s%s%s\n" $COLOR_GREEN "\n\t3. Changing default shell to zsh" $COLOR_RESET
 #chsh -s /usr/bin/zsh
