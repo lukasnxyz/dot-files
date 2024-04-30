151b81f14ee576c45012e2b97ce1107a00061a74
Lukas Nitzsche <lukasnitzsche@yahoo.com>
2024-04-30 19:53:01 +0200

    editing and testing with docker

--- install.sh
+++ install.sh
@@ -4,7 +4,21 @@ COLOR_RESET="$(tput sgr0)"
 COLOR_GREEN="$(tput setaf 2)"
 COLOR_RED="$()"
 
-# check if XDG things are set
+CONFIG_DIR="$HOME/.config/"
+CACHE_DIR="$HOME/.cache/"
+DATA_DIR="$HOME/.local/share/"
+HOME_DIR="$HOME/"
+
+mkdir -vp ${CONFIG_DIR}
+mkdir -vp ${DATA_DIR}
+mkdir -vp ${CACHE_DIR}
+mkdir -vp "${CACHE_DIR}/zsh/"
+mkdir -vp "${DATA_DIR}/nvim/undodir/"
+
+export XDG_CONFIG_HOME=${CONFIG_DIR}
+export XDG_DATA_HOME=${DATA_DIR}
+export XDG_CACHE_HOME=${CACHE_DIR}
+
 # check if laptop and if so, install auto-cpufreq?
 # (check if current dir is dot-files) git clone this repo: https://github.com/lukasnxyz/dot-files
 
@@ -15,15 +29,16 @@ packages="zsh alacritty neovim tmux"
 sudo apt install -y ${packages} # Ubuntu(apt), Arch(pacman), Fedora(dnf)
 
 printf "%s%s%s\n" $COLOR_GREEN "2. Installing configuration files" $COLOR_RESET
-configs=("alacritty/", "nvim/", ".zshenv", "zsh/", "tmux/")
-
-#packages_install=""
-#for p in "${packages[@]}"
-#do
-#	p=${p} | sed 's/.$//' # trying to remove comma
-#	echo "${p}"
-#	packages_install="${packages_install}${p} " 
-#done
+config=("alacritty/", "nvim/", "tmux/", "zsh/")
+home=(".zshenv")
+for c in "${xdg_config_home[@]}"
+do
+	cp -rv ${c} ${CONFIG_DIR}
+done
+for c in "${home[@]}"
+do
+	cp -rv ${c} ${HOME_DIR}
+done
 
 printf "%s%s%s\n" $COLOR_GREEN "3. Changing default shell to zsh" $COLOR_RESET
-#chsh -s /usr/bin/zsh
+chsh -s /usr/bin/zsh
